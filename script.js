ce2b28a5f6e0b733e42ee11d915e407c27cbd110
David8072 <136148509+David8072@users.noreply.github.com>
2023-07-27 16:32:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+console.log("Let's start the Countdown!!!");
+
+    setTimeout(function() {
+        console.log("10 sec left");
+
+        setTimeout(() => {
+            console.log("9 sec left");
+
+            setTimeout(() => {
+                console.log("8 sec left");
+
+                setTimeout(() => {
+                    console.log("7 sec left");
+
+                    setTimeout(() => {
+                        console.log("6 sec left");
+
+                        setTimeout(() => {
+                            console.log("5 sec left");
+
+                            setTimeout(() => {
+                                console.log("4 sec left");
+
+                                setTimeout(() => {
+                                    console.log("3 sec left");
+
+                                    setTimeout(() => {
+                                        console.log("2 sec left");
+
+                                        setTimeout(() => {
+                                            console.log("1 sec left");
+
+                                            console.log("Happy Independence Day!!!");
+                                        },1000);
+                                    },2000);
+                                },3000);
+                            },4000);
+                        },5000);
+                    },6000);
+                },7000);
+            },8000);
+        },9000);
+    },10000);
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
